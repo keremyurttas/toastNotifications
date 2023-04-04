dcf28df7cb37647d3f6116f9cc1e776fcf68c589
yusikerem <kerem.yky@gmail.com>
2023-04-04 15:17:38 +0300

    Fix bottomCenter container position bug.

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import toast from "./plugins/toast-notification";
 
 const app = createApp(App);
 app.use(toast, {
-  position: "topCenter",
+  position: "bottomCenter",
   duration: 4000,
 });
 app.mount("#app");
